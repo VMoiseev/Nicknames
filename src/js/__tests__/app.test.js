e4d03082eed8580a7435959c3a7d83bce654df8d
VMoiseev <e175@inbox.ru>
2021-10-12 09:16:19 +0500

    предпочтительный вариант правки по рекомендации преподавателя

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,45 +1,45 @@
 import Validator from '../app';
 
 test('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)', () => {
-  expect(new Validator('Erma_4a5-k').validateUsername()).toBe(true);
+  expect(Validator.validateUsername('Erma_4a5-k')).toBe(true);
+});
+
+test('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9) - 2й вариант', () => {
+  expect(Validator.validateUsername('йErma_45-k')).toBe(false);
 });
 
 test('Имя не должно содержать подряд более трёх цифр', () => {
-  expect(new Validator('Erma_45555-k').validateUsername()).toBe(false);
+  expect(Validator.validateUsername('Erma_45555-k')).toBe(false);
 });
 
 test('Имя может содержать подряд от одной до трёх цифр', () => {
-  expect(new Validator('Erma_455-k').validateUsername()).toBe(true);
+  expect(Validator.validateUsername('Erma_455-k')).toBe(true);
 });
 
 test('Имя не должно начинаться с цифр', () => {
-  expect(new Validator('4Erma_45-k').validateUsername()).toBe(false);
+  expect(Validator.validateUsername('4Erma_45-k')).toBe(false);
 });
 
 test('Имя не должно заканчиваться цифрой', () => {
-  expect(new Validator('Erma_45-k4').validateUsername()).toBe(false);
+  expect(Validator.validateUsername('Erma_45-k4')).toBe(false);
 });
 
 test('Имя не должно начинаться с символа подчёркивания', () => {
-  expect(new Validator('_Erma_45-k').validateUsername()).toBe(false);
+  expect(Validator.validateUsername('_Erma_45-k')).toBe(false);
 });
 
 test('Имя не должно заканчиваться символом подчёркивания', () => {
-  expect(new Validator('Erma_45-k_').validateUsername()).toBe(false);
+  expect(Validator.validateUsername('Erma_45-k_')).toBe(false);
 });
 
 test('Имя не должно начинаться с символа тире', () => {
-  expect(new Validator('-Erma_45-k').validateUsername()).toBe(false);
+  expect(Validator.validateUsername('-Erma_45-k')).toBe(false);
 });
 
 test('Имя не должно заканчиваться символом тире', () => {
-  expect(new Validator('Erma_45-k-').validateUsername()).toBe(false);
+  expect(Validator.validateUsername('Erma_45-k-')).toBe(false);
 });
 
 test('Имя не должно быть пустым', () => {
-  expect(new Validator('').validateUsername()).toBe(false);
-});
-
-test('Имя не должно содержать киррилицу', () => {
-  expect(new Validator('йErma_45-k').validateUsername()).toBe(false);
+  expect(Validator.validateUsername('')).toBe(false);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,9 @@
 export default class Validator {
-  constructor(username) {
-    this.username = username;
-  }
-
-  validateUsername() {
-    const a = /[\w-]+/.test(this.username);
-    const b = /^[^-_\W\d]/.test(this.username);
-    const c = /[^-_\W\d]$/.test(this.username);
-    const d = /\d{4,}/.test(this.username);
+  static validateUsername(username) {
+    const a = /[\w-]+/.test(username);
+    const b = /^[^-_\W\d]/.test(username);
+    const c = /[^-_\W\d]$/.test(username);
+    const d = /\d{4,}/.test(username);
     return a && b && c && !d;
   }
 }
