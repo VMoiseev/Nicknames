79025a6a03daa6d5137b7e443eb79afea1278c67
VMoiseev <e175@inbox.ru>
2021-10-14 07:49:11 +0500

    исправил недопустимые символы в середине логина

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 export default class Validator {
   static validateUsername(username) {
-    const a = /[\w-]+/.test(username);
+    const a = /[^\w-]+/.test(username);
     const b = /^[^-_\W\d]/.test(username);
     const c = /[^-_\W\d]$/.test(username);
     const d = /\d{4,}/.test(username);
-    return a && b && c && !d;
+    return !a && b && c && !d;
   }
 }
